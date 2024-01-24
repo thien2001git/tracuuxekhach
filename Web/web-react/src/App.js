f992abde5b522f8bf41ebb0f3befa82a7950add5
thien2001git <hoangthien66771508@gmail.com>
2024-01-25 00:01:49 +0700

    [Web] Thêm giao diện

--- Web/web-react/src/App.js
+++ Web/web-react/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from "react";
+import SearchIcon from "./images/Search.svg"
+import MicIcon from "./images/Mic fill.svg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHideResult: "inherit"
+        }
+    }
+
+    render() {
+        return (
+            <>
+
+                <div
+                    style={{height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                    <h1 style={{color:"#aaa"}}>TRA CỨU XE KHÁCH LIÊN TỈNH</h1>
+                    <div style={{
+                        borderRadius: "20px",
+                        boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)"
+                    }}>
+                        <div style={{
+                            width: "50vw",
+                            height: "3em",
+                            display: "flex",
+                            alignItems: "center",
+                            padding: "0px 20px"
+                        }}>
+                            <img src={SearchIcon}/>
+                            <input type={"text"}
+                                   style={{
+                                       border: "none",
+                                       outline: "none",
+                                       width: "100%",
+                                       height: "90%",
+                                       margin: "0px 10px"
+                                   }}/>
+                            <img src={MicIcon}/>
+                            <hr style={{display: this.state.isHideResult}}/>
+
+                        </div>
+                    </div>
+
+                </div>
+            </>
+        )
+    }
 }
 
 export default App;
