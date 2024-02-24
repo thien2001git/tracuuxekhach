f5336ca8b55c0312428304c490c53bdb867436d8
thien2001git <hoangthien66771508@gmail.com>
2024-02-25 00:41:19 +0700

    [BE] format MainControllerTest.kt

--- TraCuuXeKhach/src/test/kotlin/org/kimoanh/tracuuxekhach/controller/MainControllerTest.kt
+++ TraCuuXeKhach/src/test/kotlin/org/kimoanh/tracuuxekhach/controller/MainControllerTest.kt
@@ -1,13 +1,11 @@
 package org.kimoanh.tracuuxekhach.controller
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions
-
 class MainControllerTest {
-
     @Test
     fun all() {
-        Assertions.assertEquals("","")
+        Assertions.assertEquals("", "")
     }
 }
